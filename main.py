74d18a59b57c4ce608db72233c6d2620f5672d65
hieu <hieu.ngo3@mservice.com.vn>
2023-03-13 17:16:01 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+tokenizer = AutoTokenizer.from_pretrained("VietAI/vit5-large-vietnews-summarization")  
+model = AutoModelForSeq2SeqLM.from_pretrained("VietAI/vit5-large-vietnews-summarization")
+model.to("cpu")
+
+sentence = "VietAI là tổ chức phi lợi nhuận với sứ mệnh ươm mầm tài năng về trí tuệ nhân tạo và xây dựng một cộng đồng các chuyên gia trong lĩnh vực trí tuệ nhân tạo đẳng cấp quốc tế tại Việt Nam."
+text =  "vietnews: " + sentence + " </s>"
+encoding = tokenizer(text, return_tensors="pt")
+input_ids, attention_masks = encoding["input_ids"].to("cpu"), encoding["attention_mask"].to("cpu")
+outputs = model.generate(
+    input_ids=input_ids, attention_mask=attention_masks,
+    max_length=256,
+    early_stopping=True
+)
+for output in outputs:
+    line = tokenizer.decode(output, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+    print(line)
+    
